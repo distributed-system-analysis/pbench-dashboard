b35e1c33240c3c5ba50650dc96909a03eae71d59
fuwang <fuwang@redhat.com>
2020-07-23 08:32:33 -0400

    m

--- src/services/search.js
+++ src/services/search.js
@@ -4,69 +4,56 @@ import request from '../utils/request';
 export async function queryIndexMapping(params) {
   const { datastoreConfig, indices } = params;
 
-  const url = `${datastoreConfig.elasticsearch}/${datastoreConfig.prefix}${
+  const endpoint = `${datastoreConfig.elasticsearch}/${datastoreConfig.prefix}${
     datastoreConfig.run_index
   }${indices[0]}/_mappings`;
 
-  const endpoint = `${datastoreConfig.test_server}/download`;
-
-  return request.post(endpoint, {
-    data: {
-      url,
-    },
-  });
+  return request.get(endpoint);
 }
 
 export async function searchQuery(params) {
   try {
     const { datastoreConfig, selectedFields, selectedDateRange, query } = params;
 
-    const url = `${datastoreConfig.elasticsearch}/${getAllMonthsWithinRange(
+    const endpoint = `${datastoreConfig.elasticsearch}/${getAllMonthsWithinRange(
       datastoreConfig,
       datastoreConfig.run_index,
       selectedDateRange
     )}/_search`;
 
-    const endpoint = `${datastoreConfig.test_server}/download`;
-
     return request.post(endpoint, {
+      params: {
+        ignore_unavailable: true,
+      },
       data: {
-        url,
-        payload: {
-          params: {
-            ignore_unavailable: true,
-          },
-          data: {
-            size: 10000,
-            filter: {
-              range: {
-                '@timestamp': {
-                  gte: selectedDateRange.start,
-                  lte: selectedDateRange.end,
-                },
-              },
+        size: 10000,
+        filter: {
+          range: {
+            '@timestamp': {
+              gte: selectedDateRange.start,
+              lte: selectedDateRange.end,
             },
-            sort: [
-              {
-                '@timestamp': {
-                  order: 'desc',
-                  unmapped_type: 'boolean',
-                },
-              },
-            ],
-            query: {
-              filtered: {
-                query: {
-                  query_string: {
-                    query: `*${query}*`,
-                    analyze_wildcard: true,
-                  },
-                },
-              },
-            },
-            fields: selectedFields,
           },
         },
+        sort: [
+          {
+            '@timestamp': {
+              order: 'desc',
+              unmapped_type: 'boolean',
+            },
+          },
+        ],
+        query: {
+          filtered: {
+            query: {
+              query_string: {
+                query: `*${query}*`,
+                analyze_wildcard: true,
+              },
+            },
+          },
+        },
+        fields: selectedFields,
       },
     });
   } catch (error) {
