fbf980697893eda4d8bb7bdd3b2908bd9855c6ed
Aquib Baig <aquibbaig97@gmail.com>
2020-09-14 12:52:50 +0530

    reference url in the prisma data model

--- prisma/src/resolvers.js
+++ prisma/src/resolvers.js
@@ -1,7 +1,25 @@
 const bcrypt = require('bcryptjs');
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 const jwt = require('jsonwebtoken');
 
 const resolvers = {
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'Date custom scalar type',
+    parseValue(value) {
+      return new Date(value); // value from the client
+    },
+    serialize(value) {
+      return new Date(value); // value sent to the client
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10); // ast value is always in string format
+      }
+      return null;
+    },
+  }),
   Mutation: {
     login: async (parent, { username, password }, ctx) => {
       const user = await ctx.prisma.user({ username });
@@ -40,6 +58,15 @@ const resolvers = {
         password: hashedPassword,
       });
       return user;
+    },
+    createUrl: async (parent, { created, updated, config, description }, ctx) => {
+      const url = await ctx.prisma.createUrl({
+        created,
+        updated,
+        config,
+        description,
+      });
+      return url;
     },
   },
 };
