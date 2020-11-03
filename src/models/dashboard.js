86f4b1f4d1644a2a729fcb80a21270895c5bc569
Anisha1234 <aswain@redhat.com>
2020-11-03 12:37:36 +0530

    fixes

--- src/models/dashboard.js
+++ src/models/dashboard.js
@@ -151,7 +151,6 @@ export default {
         extension,
         fileNames,
       };
-      console.log(summaryTocResult);
       yield put({
         type: 'getTocResult',
         payload: summaryTocResult,
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -79,8 +79,10 @@ export const insertTocTreeData = (tocResult, items = [], [head, ...tail]) => {
     }
   }
   let child = items.find(childNode => childNode.name === head);
+  console.log(fileData, head, child);
   if (!child) {
     if (fileData[head] !== undefined) {
+      console.log('pushing size and mode');
       items.push(
         (child = {
           name: head,
