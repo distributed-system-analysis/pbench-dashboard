136d95bfbbd24682d0c3893b3c9351ea6a101edc
gurbirkalsi <kalsigurbir@gmail.com>
2021-02-04 16:15:44 -0500

    Handle table pagination

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -7,7 +7,7 @@ import {
   sortable,
   SortByDirection,
 } from '@patternfly/react-table';
-import { Spinner } from '@patternfly/react-core';
+import { Spinner, Pagination } from '@patternfly/react-core';
 import SearchBar from '../SearchBar';
 
 export default class Table extends PureComponent {
@@ -16,31 +16,37 @@ export default class Table extends PureComponent {
 
     const { dataSource, columns, actions } = props;
 
+    this.defaultPerPage = 20;
+    this.defaultRows = this.parseRows(dataSource);
     this.state = {
       cells: this.parseColumns(columns),
-      rows: this.parseRows(dataSource),
+      rows: this.defaultRows.slice(0, this.defaultPerPage),
       actions,
       sortBy: {},
+      page: 1,
+      perPage: this.defaultPerPage,
     };
   }
 
-  parseColumns = columns => {
-    const parsedColumns = columns.map(column => {
-      return {
-        title: column.title,
-        transforms: [sortable],
-      };
+  onSetPage = (event, newPage, perPage, startIdx, endIdx) => {
+    const { dataSource } = this.props;
+    const rows = this.parseRows(dataSource);
+
+    this.setState({
+      page: newPage,
+      rows: rows.slice(startIdx, endIdx),
     });
-    return parsedColumns;
   };
 
-  parseRows = rows => {
-    const parsedRows = rows.map(row => {
-      return {
-        cells: Object.values(row),
-      };
+  onPerPageSelect = (event, newPerPage, newPage, startIdx, endIdx) => {
+    const { dataSource } = this.props;
+    const rows = this.parseRows(dataSource);
+
+    this.setState({
+      perPage: newPerPage,
+      page: newPage,
+      rows: rows.slice(startIdx, endIdx),
     });
-    return parsedRows;
   };
 
   onSelect = (event, isSelected, rowId) => {
@@ -103,13 +109,50 @@ export default class Table extends PureComponent {
     onRowClick(row[0]);
   };
 
+  parseColumns = columns => {
+    const parsedColumns = columns.map(column => {
+      return {
+        title: column.title,
+        transforms: [sortable],
+      };
+    });
+    return parsedColumns;
+  };
+
+  parseRows = rows => {
+    const parsedRows = rows.map(row => {
+      return {
+        cells: Object.values(row),
+        favorited: true,
+      };
+    });
+    return parsedRows;
+  };
+
   render() {
     const { loading, ...childProps } = this.props;
-    const { cells, rows, actions, sortBy } = this.state;
+    const { cells, rows, actions, sortBy, page, perPage } = this.state;
 
     return (
       <React.Fragment>
         <SearchBar style={{ marginRight: 32 }} placeholder="Search" onSearch={this.onSearch} />
+        <Pagination
+          isCompact
+          itemCount={this.defaultRows.length}
+          page={page}
+          perPage={perPage}
+          onSetPage={this.onSetPage}
+          onPerPageSelect={this.onPerPageSelect}
+          perPageOptions={[
+            { title: '20', value: 20 },
+            { title: '50', value: 50 },
+            { title: '100', value: 100 },
+            { title: '200', value: 200 },
+          ]}
+          titles={{
+            paginationTitle: `top pagination`,
+          }}
+        />
         <PatternFlyTable
           aria-label="Table"
           variant={TableVariant.compact}
