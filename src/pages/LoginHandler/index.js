bf8c3bf2839dc39b849d71683bc95f742cd25ee9
Aquib Baig <aquibbaig97@gmail.com>
2021-03-08 08:41:36 -0500

    handle edge cases for the login page
    show appropriate message on success or failure
    of the login process, redirects to the overview page
    on success.

--- src/pages/LoginHandler/index.js
+++ src/pages/LoginHandler/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Form,
   FormGroup,
@@ -7,33 +7,31 @@ import {
   ActionGroup,
   Button,
   Title,
+  Alert,
+  AlertActionCloseButton,
 } from '@patternfly/react-core';
 import AuthLayout from '@/components/AuthLayout';
 import { routerRedux } from 'dva/router';
 import { connect } from 'dva';
 import styles from './index.less';
 
-@connect(auth => ({
-  auth: auth.auth,
-}))
-class LoginHandler extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      variantVal: 'tertiary',
-      btnColor: 'black',
-      btnDisabled: 'true',
-    };
-  }
+const mapStateToProps = state => {
+  const { auth } = state;
+  return auth;
+};
 
-  componentDidMount = () => {
-    this.disableSubmitBtn();
-  };
+const LoginHandler = props => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertComponent, setAlertComponent] = useState({
+    message: '',
+    variant: '',
+  });
 
-  setLoggedIn = payload => {
-    const { dispatch } = this.props;
+  const setLoggedIn = payload => {
+    const { dispatch } = props;
     dispatch({
       type: 'auth/loadUser',
       payload,
@@ -42,114 +40,103 @@ class LoginHandler extends React.Component {
     });
   };
 
-  enableSubmitBtn = () => {
-    this.setState({
-      variantVal: 'primary',
-      btnColor: 'white',
-      btnDisabled: 'false',
-    });
-  };
-
-  disableSubmitBtn = () => {
-    this.setState({
-      variantVal: 'tertiary',
-      btnColor: 'black',
-      btnDisabled: 'true',
-    });
-  };
-
-  handleUserNameInputChange = username => {
-    const { password } = this.state;
-    this.setState({
-      username,
-    });
-    if (password !== '' && username !== '') {
-      this.enableSubmitBtn();
-    } else {
-      this.disableSubmitBtn();
-    }
-  };
-
-  handlePassWordInputChange = password => {
-    const { username } = this.state;
-    this.setState({
-      password,
-    });
-    if (username !== '' && password !== '') {
-      this.enableSubmitBtn();
-    } else {
-      this.disableSubmitBtn();
-    }
-  };
-
-  handleLoginSubmit = () => {
-    const { username, password } = this.state;
-    const { dispatch } = this.props;
-    dispatch({
+  const handleLoginSubmit = async () => {
+    const { dispatch } = props;
+    const response = await dispatch({
       type: 'auth/loginUser',
       payload: {
         username,
         password,
       },
-    }).then(response => {
-      const { status } = response;
-      if (status === 'success') {
-        this.setLoggedIn({ username });
-      }
     });
+    const { status, message } = response;
+    setAlertComponent({
+      message,
+      variant: status === 'failure' ? 'danger' : 'success',
+    });
+    setAlertOpen(true);
+    if (status === 'success') {
+      // wait for a couple
+      // of seconds before taking the
+      // user to the overview page.
+      setTimeout(() => {
+        setLoggedIn({ username });
+      }, 2000);
+    }
   };
 
-  render() {
-    const { variantVal, btnColor, btnDisabled } = this.state;
-    const form = (
-      <div className={styles.section}>
-        <Form>
-          <FormGroup label="Email address" isRequired fieldId="horizontal-form-name">
-            <TextInput
-              isRequired
-              type="text"
-              id="horizontal-form-name"
-              aria-describedby="horizontal-form-name-helper"
-              name="horizontal-form-name"
-              onChange={this.handleUserNameInputChange}
-            />
-          </FormGroup>
-          <FormGroup label="Password" isRequired fieldId="horizontal-form-password">
-            <TextInput
-              isRequired
-              type="password"
-              id="horizontal-form-password"
-              name="horizontal-form-password"
-              onChange={this.handlePassWordInputChange}
-            />
-          </FormGroup>
-          <FormGroup fieldId="remember-me">
-            <Checkbox
-              label="Keep me logged in"
-              id="alt-form-checkbox-1"
-              name="alt-form-checkbox-1"
-              className={styles.check}
-            />
-          </FormGroup>
-          <ActionGroup>
-            <Button
-              isBlock
-              variant={variantVal}
-              onClick={() => this.handleLoginSubmit()}
-              className={styles.btn}
-              id="submitBtn"
-              {...(btnDisabled === 'true' ? { isDisabled: 'true' } : {})}
-            >
-              <Title headingLevel="h4" size="xl" style={{ color: btnColor }}>
-                Submit
-              </Title>
-            </Button>
-          </ActionGroup>
-        </Form>
-      </div>
-    );
-    return <AuthLayout toPreview={form} heading="Log into your Pbench Acount" backOpt="true" />;
-  }
-}
+  useEffect(
+    () => {
+      if (username.trim() !== '' && password.trim() !== '') {
+        setBtnDisabled(false);
+      }
+    },
+    [username, password]
+  );
 
-export default LoginHandler;
+  const form = (
+    <Form className={styles.section}>
+      {alertOpen ? (
+        <Alert
+          title={alertComponent.message}
+          variant={alertComponent.variant}
+          action={<AlertActionCloseButton onClose={() => setAlertOpen(!alertOpen)} />}
+        />
+      ) : (
+        <></>
+      )}
+      <FormGroup label="Email address" isRequired fieldId="horizontal-form-name">
+        <TextInput
+          isRequired
+          type="text"
+          id="horizontal-form-name"
+          aria-describedby="horizontal-form-name-helper"
+          name="horizontal-form-name"
+          onChange={val => setUsername(val)}
+        />
+      </FormGroup>
+      <FormGroup label="Password" isRequired fieldId="horizontal-form-password">
+        <TextInput
+          isRequired
+          type="password"
+          id="horizontal-form-password"
+          name="horizontal-form-password"
+          onChange={val => setPassword(val)}
+        />
+      </FormGroup>
+      <FormGroup fieldId="remember-me">
+        <Checkbox
+          label="Keep me logged in"
+          id="alt-form-checkbox-1"
+          name="alt-form-checkbox-1"
+          className={styles.check}
+        />
+      </FormGroup>
+      <ActionGroup>
+        <Button
+          isBlock
+          // variant={variantVal}
+          onClick={handleLoginSubmit}
+          className={styles.btn}
+          id="submitBtn"
+          {...(btnDisabled ? { isDisabled: true } : { isDisabled: false })}
+        >
+          <Title
+            headingLevel="h4"
+            size="xl"
+            style={btnDisabled ? { color: 'black' } : { color: 'white' }}
+          >
+            Submit
+          </Title>
+        </Button>
+      </ActionGroup>
+    </Form>
+  );
+  return (
+    <div>
+      <AuthLayout toPreview={form} heading="Log into your Pbench Acount" backOpt="true" />
+    </div>
+  );
+};
+
+export default connect(mapStateToProps)(LoginHandler);
