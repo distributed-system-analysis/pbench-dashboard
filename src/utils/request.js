a7a2ec96d10a92ed7f9aa4d5940bddde1dc8e373
Aquib Baig <aquibbaig97@gmail.com>
2021-03-15 00:39:42 -0400

    modify umi-request
    removes request abstrations for errorhandler to just throw errors
    so that we could custom handle them on a component level

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,54 +1,30 @@
 import { extend } from 'umi-request';
-import { notification } from 'antd';
-import router from 'umi/router';
-
-const codeMessage = {
-  200: 'The server successfully returned the requested data. ',
-  201: 'New or modified data is successful. ',
-  202: 'A request has entered the background queue (asynchronous task). ',
-  204: 'The data was deleted successfully. ',
-  400: 'The request returned an error and the server did not perform any new or modified data operations. ',
-  401: 'User does not have permission (token, username, or password is incorrect). ',
-  403: 'The user is authorized, but access is forbidden. ',
-  404: 'The request was made for a record that does not exist. ',
-  406: 'The format of the request is not available. ',
-  410: 'The requested resource is permanently deleted and will not be retrieved. ',
-  422: 'A validation error occurred when creating an object. ',
-  500: 'An error occurred on the server. Please check the server. ',
-  502: 'Gateway error. ',
-  503: 'The service is unavailable and the server is temporarily overloaded or maintained. ',
-  504: 'The gateway timed out. ',
-};
-
-const errorHandler = error => {
-  const { response = {} } = error;
-  const errortext = codeMessage[response.status] || response.statusText;
-  const { status } = response;
-
-  notification.error({
-    message: `Request Error ${status}`,
-    description: errortext,
-  });
-
-  if (status === 403) {
-    router.push('/exception/403');
-    return;
-  }
-  if (status === 404) {
-    router.push('/exception/404');
-    return;
-  }
-  if (status <= 504 && status >= 500) {
-    router.push('/exception/500');
-    return;
-  }
-  if (status >= 405 && status < 422) {
-    router.push('/exception/404');
-  }
-};
 
 const request = extend({
-  errorHandler, // extend default error handler with custom actions
+  errorHandler: error => {
+    throw error;
+  },
+});
+
+// Embeds authorization token with every request.
+request.interceptors.request.use((url, options) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    return {
+      url,
+      options: {
+        ...options,
+        headers: {
+          ...options.headers,
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    };
+  }
+  return {
+    url,
+    options,
+  };
 });
 
 export default request;
